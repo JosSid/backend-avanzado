2e94f8419608f243d6fbb6e54256643de1349018
Josep Valls <alfredvays@gmail.com>
2022-11-30 12:50:54 +0100

    creamos servidos para implementar websockets

---
+++ ejemplos/ejemplo_websockets/httpServer.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+
+app.use('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+    console.log('Servidor arrancado en http://localhost:3000')
+});
